82180b11049ac12735479957bbf8f960f2b05db0
ocarina51006@gmail.com <ocarina51006@gmail.com>
2023-08-07 17:08:00 +0800

    8/7

---
+++ slnAJAXDemo/prjAJAXDemo/Controllers/apiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjAJAXDemo.Controllers
+{
+    public class apiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello AJAX"); 
+        }
+    }
+}
